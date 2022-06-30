c497a0607ba6dabe39c239c74ada5eaafd337a9d
Olga Galkina <ole4kafaleeva@rambler.ru>
2022-06-30 14:58:14 +0400

    task_2 done

--- task_2/solution.js
+++ task_2/solution.js
@@ -2,6 +2,14 @@ function calcShipping(sum, min, shipping) {
     let productsSum = sum; // сумма в корзине
     let freeShippingMinSum = min; // минимальная цена для бесплатной доставки
     let shippingPrice = shipping; // стоимость доставки
+    let shippingSum;
+    if(productsSum = 0){
+        shippingSum = 0; 
+    }
+    if(productsSum >= freeShippingMinSum ){
+        shippingSum = 0; 
+    }
+    else shippingSum = shippingPrice;
 
     // Задание №2.1. Рассчитать доставку
 
@@ -25,6 +33,11 @@ function calcDiscount(sum, min, discount) {
     let productsSum = sum; // сумма в корзине
     let discountMinSum = min; // минимальная цена для скидки
     let discountPart = discount; // величина скидки в процентах
+    let discountSum;
+    if(productsSum >= discountMinSum){
+        discountSum =  (discountPart * productsSum)/100;
+    }
+    else discountSum = 0;
 
     // Задание №2.2. Рассчитать скидку
 
@@ -42,6 +55,8 @@ function calcDiscount(sum, min, discount) {
 function calcInvoice({sum, discountMinSum, discountPart, shippingFreeMinSum, shippingPrice}) {
     let productsSum = sum;
     let discountSum = calcDiscount(sum, discountMinSum, discountPart);
+    let totalSum = productsSum;
+        totalSum = totalSum - discountSum;
 
     // Задача №2.3. Рассчитать скидки и доставку в корзине
 
@@ -51,13 +66,14 @@ function calcInvoice({sum, discountMinSum, discountPart, shippingFreeMinSum, shi
     // уменьшите totalSum на discountSum
 
     let shippingSum = calcShipping(totalSum, shippingFreeMinSum, shippingPrice); // не изменяйте эту строку!!!
-
+        totalSum = totalSum + shippingSum;
     // прибавьте к totalSum значение shippingSum
 
     // создайте переменную freeShipping
     // запишите без использования if или любых других условий:
     // если shippingSum равно нулю, то freeShipping должна быть равна true, иначе freeShipping должна быть равна false
-
+    let freeShipping;
+    (shippingSum == 0) ? freeShipping = true : freeShipping = false;
     // Конец решения задачи №2.3.
 
     return {discount: discountSum, freeShipping, shipping: shippingSum, total: totalSum};
