0e1a23cb3fd92120d5eebccd5c9e87e6451bb9e7
Olga Galkina <ole4kafaleeva@rambler.ru>
2022-07-12 15:17:41 +0400

    task4 done

--- task_4/solution.js
+++ task_4/solution.js
@@ -1,6 +1,9 @@
 // Задание 4.1. Вывод карточек товаров
 
 function parseProducts(json) {
+    let data = JSON.parse(json);
+    let products = data.products;
+    return products;
     // Аргументом функции является JSON
 
     // Преобразуйте строку json, переданную как аргумент функции,
@@ -8,7 +11,16 @@ function parseProducts(json) {
     // и запишите в переменную data
     // Верните как результат функции свойство products объекта data
 }
+function renderProductsCards(json){
+    clearProducts();
+    parseProducts(json);
+   let  products = parseProducts(json);  
+   let length = products.length;
+   for (let i = 0; i < length; i = i+1) {
+    addProduct(products[i]);  
+ }
 
+}
 // Напишите функцию renderProductsCards(json)
 // Аргументом функции является JSON
 // Выполните функцию clearProducts - она уже написана в коде, просто вызовите ее, аргументы не требуются
