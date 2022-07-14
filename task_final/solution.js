86d29165a69ac8486f752d707c49bbe1c2458555
Olga Galkina <ole4kafaleeva@rambler.ru>
2022-07-14 10:26:13 +0400

    task_final done

--- task_final/solution.js
+++ task_final/solution.js
@@ -34,20 +34,32 @@
 // }
 
 function sendRequest(name, phone, address, goods, sum) {
-    let data = {goods: [], order: {}};
+    let data = {client: String,
+        order:{
+            address: String,
+            sum: Number
+            },
+            goods: []          
+               };
+     //data.goods = {title: String, count: Integer};
+     //data.order.address = {street: " ", house:" ", entrance:" ", floor:" ", flat:" "};
+    //data.client.phone = `${ phone}`;
+    //data.client =  name + phone;
+        
 
     let countOfGoods = goods.length;
 
-    for (let i = 0; i <= countOfGoods; i += 1) {
-        data.goods.push(goods[i].title);
+    for (let i = 0; i < countOfGoods; i += 1) {
+        let tempGoods = {title : goods[i].title, count: goods[i].count}
+        data.goods.push(tempGoods)   
     }
 
-    data.order.address = address;
-    data.order.sum = name + phone + address + goods + sum;
+    data.order.address = `ул. ${address.street}, дом ${address.house}, ${address.entrance} подъезд, ${address.floor} этаж, кв ${address.flat}`;
+    data.order.sum = sum;
 
-    data.client = 'Иван';
+    data.client = name + " " + phone;
 
-    let jsonData = JSON.stringify(data);
+    let jsonData = JSON.stringify({data});
 
     return jsonData;
 }
